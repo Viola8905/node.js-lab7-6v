fcfb3cef7e5558b85afeaf471558f6cc9b504ebe
Viola Prokopchuck <74563100+Viola8905@users.noreply.github.com>
2021-12-20 19:04:46 +0200

    Delete lab7 directory

--- lab7/backend/src/options.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  backendPort: 3000,
-  retryInterval: 1000,
-  dbPath: process.env.NODE_ENV == "test" ?  "./backend/data/Abonent.db": "./data/Abonent.db",
-  frontendPort: 5000,
-  frontendIP: "127.0.0.42",
-}
--- lab7/frontend/for_devs/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express").Router;
-const devController = require("./controller.js");
-
-const devRouter = new Router();
-
-devRouter.get("/technical-information", devController.getAPI);
-devRouter.get("/author-information", devController.getAuthor);
-
-module.exports = devRouter;
